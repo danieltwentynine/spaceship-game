73c9e3ad5959aad800b6127392badf8358482fb5
danieltwentynine <danielmcardoso2016@protonmail.com>
2024-10-15 08:17:43 -0300

    adicionando funcionalidade de restart e quit

--- main.py
+++ main.py
@@ -73,6 +73,8 @@ def show_score(x, y):
 def game_over_text(x, y):
     go = go_font.render("GAME OVER", True, (255, 255, 255))
     screen.blit(go, (200, 250))
+    restart = font.render("Press R to Restart or Q to Quit", True, (255, 255, 255))
+    screen.blit(restart, (200, 350))
     
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -95,9 +97,22 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
     else:
         return False
 
-running = True
-while running:
+def reset_game():
+    global score_value, playerX, playerY, bulletX, bulletY, bullet_state
+    score_value = 0
+    playerX = 370
+    playerY = 480
+    bulletX = 0
+    bulletY = 480
+    bullet_state = "ready"
+    for i in range(num_of_enemies):
+        enemyX[i] = random.randint(0, 735)
+        enemyY[i] = random.randint(50, 150)
     
+running = True
+game_over = False
+
+while running:
     #screen background color
     screen.fill((0,0,0))
     #background image
@@ -120,65 +135,74 @@ while running:
                     #getting the x coordinate of the spaceship
                     bulletX = playerX
                     fire_bullet(bulletX, bulletY)
+            if game_over:
+                if event.key == pygame.K_r:
+                    game_over = False
+                    reset_game()
+                elif event.key == pygame.K_q:
+                    running = False
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
                 playerY_change = 0
             
-    playerX += playerX_change
-    
-    #adding screen hitbox
-    
-    #player inside window boundary
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
-        playerX = 736
+    if not game_over:
+        playerX += playerX_change
         
-    #enemy movement
-    for i in range(num_of_enemies):
+        #adding screen hitbox
         
-        #game over
-        if enemyY[i] > 440:
-            for j in range(num_of_enemies):
-                enemyY[j] = 2000
-            game_over_text(textX, textY)
-            break
-        
-        enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 5
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 736:
-            enemyX_change[i] = -5
-            enemyY[i] += enemyY_change[i]
+        #player inside window boundary
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
             
-        #Collision
-        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
-        if collision:
-            #creating collision sound
-            burst(enemyX[i], enemyY[i])
-            explosion = mixer.Sound('explosion.wav')
-            explosion.play()
+        #enemy movement
+        for i in range(num_of_enemies):
+            
+            #game over
+            if enemyY[i] > 440:
+                for j in range(num_of_enemies):
+                    enemyY[j] = 2000
+                game_over = True
+                break
+            
+            enemyX[i] += enemyX_change[i]
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 5
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 736:
+                enemyX_change[i] = -5
+                enemyY[i] += enemyY_change[i]
+                
+            #Collision
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+                #creating collision sound
+                burst(enemyX[i], enemyY[i])
+                explosion = mixer.Sound('explosion.wav')
+                explosion.play()
+                bulletY = 480
+                bullet_state = "ready"
+                score_value += 1
+                enemyX[i] = random.randint(0, 735)
+                enemyY[i] = random.randint(50, 150)
+            
+            enemy(enemyX[i], enemyY[i], i)
+            
+        #bullet movement
+        if bulletY <= 0:
             bulletY = 480
             bullet_state = "ready"
-            score_value += 1
-            enemyX[i] = random.randint(0, 735)
-            enemyY[i] = random.randint(50, 150)
         
-        enemy(enemyX[i], enemyY[i], i)
-        
-    #bullet movement
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = "ready"
-    
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
-        
-    player(playerX, playerY)
-    show_score(textX, textY)
+        if bullet_state is "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletY -= bulletY_change
+            
+        player(playerX, playerY)
+        show_score(textX, textY)
+    else:
+        game_over_text(textX, textY)
     
     #updating display
     pygame.display.update()
